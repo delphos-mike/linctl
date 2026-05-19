2b3444d9571a635fc5037cc0d128f996c1226baf
Mike Beaumier <mike@delphoslabs.com>
2026-05-19 14:28:31 -0700

    chore: update Homebrew formula to point to fork
    - Change homepage, url, head to delphos-mike/linctl
    - Update branch from master to main
    - Update LDFLAGS module path
    - Bump version reference to v0.3.0
    - Comment out sha256 (pending release tag creation)

--- Formula/linctl.rb
+++ Formula/linctl.rb
@@ -1,15 +1,15 @@
 class Linctl < Formula
   desc "Comprehensive command-line interface for Linear's API"
-  homepage "https://github.com/dorkitude/linctl"
-  url "https://github.com/dorkitude/linctl/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "7aea99cc1bee2f097020930e0cc9e7a575340ab4969e81d673299a60ad586874"
+  homepage "https://github.com/delphos-mike/linctl"
+  url "https://github.com/delphos-mike/linctl/archive/refs/tags/v0.3.0.tar.gz"
+  # sha256 - update after v0.3.0 release tag is created
   license "MIT"
-  head "https://github.com/dorkitude/linctl.git", branch: "master"
+  head "https://github.com/delphos-mike/linctl.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/dorkitude/linctl/cmd.version=#{version}")
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/delphos-mike/linctl/cmd.version=#{version}")
   end
 
   test do
